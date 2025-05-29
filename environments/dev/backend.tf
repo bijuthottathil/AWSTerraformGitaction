91c765b5386fe0a2ec233336d7c41ffd265771f9
Biju Mathew Thottathil <63364506+bijuthottathil@users.noreply.github.com>
2025-05-29 09:47:29 -0500

    Delete environments/dev/backend.tf

--- environments/dev/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "my-terraform-state-bucket-biju"
-    key            = "s3-bucket-terraform/dev/terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-  }
-}
